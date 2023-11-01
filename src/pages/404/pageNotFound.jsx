4a29ce3cbaee199e33da97d032f4e3d6a665df52
Kulveer Singh <kulveer.coder@gmail.com>
2023-11-01 22:32:02 +0530

    pages created

---
+++ src/pages/404/pageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.scss";
+
+const pageNotFound = () => {
+  return (
+    <div>
+      Page not Found
+    </div>
+  )
+}
+
+export default pageNotFound;
+
---
+++ src/pages/searchResult/searchresult.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.scss";
+
+const searchresult = () => {
+  return (
+    <div>
+      Search Result
+    </div>
+  )
+}
+
+export default searchresult;
+
